fe8054a09df1a20a3b6be8284a45230fbec8d2d9
Priyanshu Jha <129042608+priyanshujhagithub@users.noreply.github.com>
2025-07-14 16:47:13 +0530

    Delete server directory

--- server/routes/returnRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { createReturn } from "../controllers/returnController.js"
-import { protect } from "../middleware/authMiddleware.js"
-
-const router = express.Router()
-
-router.post("/return", protect, createReturn)
-
-export default router
